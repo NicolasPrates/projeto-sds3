d3c2c445821cde270391a081815c421ccbd4a149
NicolasPrates <nicolaspratesaraujo@gmail.com>
2021-11-08 10:03:53 -0300

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-      <footer className="footer mt-auto py-3 bg-dark">
+      <footer className="fixed-bottom bg-dark">
         <div className="container">
           <p className="text-light">App desenvolvido por <a href="https://github.com/NicolasPrates" target="_blank" rel="noreferrer">Nicolas Prates</a></p>
           <p className="text-light"><small><strong>Semana Spring React</strong><br/>
